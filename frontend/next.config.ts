f37493b42fdba0eb30a3b386b254de43c99c9de0
Kne34 <wkeane004@gmail.com>
2026-08-17 23:23:44 +0700

    chore: update docker compose and next.js configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+  turbopack: {},
   async rewrites() {
     return [
       {
